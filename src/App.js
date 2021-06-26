a19b3d3bcedf7b0da8a20a447df6fe86aea71428
Peago <dongoctien97@gmail.com>
2021-06-26 09:31:35 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,312 @@
 import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+import { Row, Col, Divider } from 'antd';
+
 
 function App() {
+  const { innerWidth: width, innerHeight: height } = window;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Row align={'middle'} >
+        <Col span={24}>
+          <h2 style={{ textAlign: 'center', backgroundColor: 'red' }}>English Exam</h2>
+        </Col>
+
+      </Row>
+      <Row gutter={16} style={{ padding: `${height / 100 * 5}px ${height / 100 * 5}px` }}>
+        <Col span={11} style={{ height: height / 100 * 80, overflow: 'scroll' }} className="gutter-row">
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+
+
+
+        </Col>
+        <Col span={2}>
+          <Divider />
+        </Col>
+        <Col span={11} className="gutter-row" style={{ height: height / 100 * 80, overflow: 'scroll' }}>
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+
+          <p>
+            “Life is what happens when you’re busy making other plans.” —
+        <strong>John Lennon</strong>
+          </p>
+        </Col>
+      </Row>
+    </>
   );
 }
 
